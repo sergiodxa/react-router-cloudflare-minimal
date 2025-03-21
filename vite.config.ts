335af479a34f84262e38562d63f7c53ebfb5133f
Sergio Xalambrí <hello@sergiodxa.com>
2025-03-20 23:40:39 -0500

    Refactor Vite configuration to remove SSR build condition from rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
+export default defineConfig({
   server: { port: 3000 },
-  build: {
-    rollupOptions: isSsrBuild ? { input: "./app/entry.worker.ts" } : undefined,
-  },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
   ],
-}));
+});
